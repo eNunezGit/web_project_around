d5695ae7890b882871c9516e00f53e2709889e37
KikeNunez <enrique.nunez.atiaga@gmail.com>
2025-12-04 17:40:23 -0600

    deleted popupHandler and hidePopup functions

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,30 +1,3 @@
-export function popupHandler(popup, submitButton, functionToRun = hidePopup) {
-    document.addEventListener('keydown', (e) => {
-        if (e.key === "Escape") {
-            functionToRun(popup);
-        }
-    });
-    popup.querySelector('.popup__close-button').addEventListener('click', () => functionToRun(popup));
-    popup.querySelector('.popup__overlay').addEventListener('dblclick', () => functionToRun(popup));
-    if (submitButton) {
-        submitButton.addEventListener('click', () => {
-            functionToRun(popup);
-        });
-    }
-};
-
-export function hidePopup(popup) {
-    if (popup.style.display === 'block') {
-        popup.style.display = 'none';
-    }
-}; 
-
-export function imgDisplay(e) {
-    const parent = e.target.parentElement;
-    const imgPopup = parent.querySelector('#imgDisplay');
-    imgPopup.style.display = 'block';
-}
-
 export function deleteCard(e) {
     const card = e.target.closest('.elements__card');
     if (card) {
