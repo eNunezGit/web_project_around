264ca880b55f459cbc2e35308414511d2a5caa4b
KikeNunez <enrique.nunez.atiaga@gmail.com>
2026-04-22 17:15:54 -0600

    refactor: changed identation

--- scripts/Api.js
+++ scripts/Api.js
@@ -1,38 +1,38 @@
 class Api {
-  constructor({baseUrl, headers}) {
-    this._baseUrl = baseUrl;
-    this._headers = headers;
-  }
+    constructor({baseUrl, headers}) {
+        this._baseUrl = baseUrl;
+        this._headers = headers;
+    }
 
-  getUserInfo(){
-    return fetch(`${this._baseUrl}/users/me`, {
-      method: "GET",
-      headers: this._headers
-    })
-    .then(res => {
-      if (res.ok) {
-        return res.json();
-      }
-      return Promise.reject(`Error: ${res.status} ${res.statusText}`);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-  }
+    getUserInfo(){
+        return fetch(`${this._baseUrl}/users/me`, {
+            method: "GET",
+            headers: this._headers
+        })
+        .then(res => {
+            if (res.ok) {
+                return res.json();
+            }
+            return Promise.reject(`Error: ${res.status} ${res.statusText}`);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    }
 
-  getInitialCards() {
-    // ...
-  }
+    getInitialCards() {
+        // ...
+    }
 
   // otros métodos para trabajar con la API
 }
 
 const api = new Api({
-  baseUrl: "https://around-api.es.tripleten-services.com/v1",
-  headers: {
-    authorization: "b100bf75-a46c-483c-9b28-27beb1ac7303",
-    "Content-Type": "application/json"
-  }
+    baseUrl: "https://around-api.es.tripleten-services.com/v1",
+    headers: {
+        authorization: "b100bf75-a46c-483c-9b28-27beb1ac7303",
+        "Content-Type": "application/json"
+    }
 });
 
 export default api;
