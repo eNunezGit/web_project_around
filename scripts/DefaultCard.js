385bdd5834e13160fbe9b92d280bf05080425626
KikeNunez <enrique.nunez.atiaga@gmail.com>
2026-05-01 12:30:37 -0600

    feat: added constructor handlers for delete and like buttons & like display based on isLiked property

--- scripts/DefaultCard.js
+++ scripts/DefaultCard.js
@@ -1,7 +1,5 @@
-import {deleteCard, likeCard} from "./utils.js";
-
 export class DefaultCard {
-    constructor({cardTitle, cardImg, cardId, likeVal, ownerId, handleCardClick}) {
+    constructor({cardTitle, cardImg, cardId, likeVal, ownerId, handleCardClick, handleDeleteClick, handleLikeClick}) {
         this.cardTitle = cardTitle;
         this.cardImg = cardImg;
         this.cardId = cardId;
@@ -9,14 +7,16 @@ export class DefaultCard {
         this.ownerId = ownerId;
 
         this._handleCardClick = handleCardClick;
+        this._handleDeleteClick = handleDeleteClick;
+        this._handleLikeClick = handleLikeClick;
     }
     
     _setEventListeners(cardElement) {
         cardElement.querySelector('.elements__img').addEventListener('click', this._handleCardClick);
 
-        cardElement.querySelector('.elements__delete-button-img').addEventListener('click', deleteCard);
+        cardElement.querySelector('.elements__delete-button-img').addEventListener('click', this._handleDeleteClick);
 
-        cardElement.querySelector('.elements__like-button-img').addEventListener('click', likeCard);
+        cardElement.querySelector('.elements__like-button-img').addEventListener('click', this._handleLikeClick);
     }
 
     _buildCard(cardTitle, cardImg) {
@@ -28,6 +28,10 @@ export class DefaultCard {
     cardElement.querySelector('.elements__img').src = cardImg;
     cardElement.querySelector('.elements__img').alt = cardTitle;
     cardElement.querySelector('.elements__title').textContent = cardTitle;
+
+    cardElement.querySelector('.elements__like-button-img').src = this.isLiked
+    ? '../images/like-button_full.svg'
+    : '../images/like-button.svg';
 
     this._setEventListeners(cardElement);
 
